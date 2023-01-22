ccb5565f0a41566e56a43c4c1724e3d4cd50f8e9
Mario Galan <mariogalansoto@hotmail.com>
2023-01-21 21:03:36 -0400

    fill assets folder and create consts files

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+// import a from './assets/profile-image';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Start working on your app!</Text>
+      {/* <Image source={require('./assets')}></Image> */}
       <StatusBar style="auto" />
     </View>
   );
---
+++ const/pets.js
@@ -0,0 +1,21 @@
+const pets = [
+    {
+        pet : 'cats',
+        pets:[
+            {
+                id:'1',
+                name:'tommy',
+                image: require('../assets/cat-image'),
+                type: 'chausie',
+                age: '4 years old'
+            },
+        ],
+
+        // pet:'dog',
+        // pets:[
+        //     {
+        //         id
+        //     }
+        // ],
+    }
+]
